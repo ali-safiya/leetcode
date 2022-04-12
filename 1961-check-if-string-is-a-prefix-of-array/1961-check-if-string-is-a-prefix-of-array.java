92666749ec32fa0732ac7cd20931cd59c8afd18a
ali-safiya <91785905+ali-safiya@users.noreply.github.com>
2022-04-12 22:14:55 +0530

    Time: 3 ms (31.98%), Space: 43.4 MB (34.75%) - LeetHub

---
+++ 1961-check-if-string-is-a-prefix-of-array/1961-check-if-string-is-a-prefix-of-array.java
@@ -0,0 +1,13 @@
+class Solution {
+    public boolean isPrefixString(String s, String[] words) {
+        String ans="";
+        
+        for(int i=0;i< words.length;i++){
+            ans+=words[i];
+           
+            if(ans.equals(s))
+                return true;
+        }
+        return false;
+    }
+}
