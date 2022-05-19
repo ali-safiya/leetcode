e3bdcaadc497921589b541fa31bf7877471612ec
ali-safiya <91785905+ali-safiya@users.noreply.github.com>
2022-05-19 22:16:21 +0530

    Time: 0 ms (100.00%), Space: 41.1 MB (34.46%) - LeetHub

--- 62-unique-paths/62-unique-paths.java
+++ 62-unique-paths/62-unique-paths.java
@@ -1,22 +1,24 @@
 class Solution {
     public int uniquePaths(int m, int n) {
-        
-        int max = Math.max(m,n);
-        int min=  Math.min(m,n);
-        
-        long noOfPaths = 1;
-        
-        for(int i= max ;i<(max+min-1) ;i++  ){
-            
-            noOfPaths*=i;
-            noOfPaths/=(i-max+1);
-        }
-
-        return (int)noOfPaths;
-        
-        
-        
+       int[][] dp= new int[m+1][n+1];
+        for(int[] arr: dp)
+        Arrays.fill(arr,-1);
+        return  findWays(dp,m,n);
+     
+       
     }
-    
-   
+    int findWays(int[][] dp,int m,int n){
+       
+        if(dp[m][n]!=-1) return dp[m][n];
+          
+        if(m==1 || n==1)
+            return 1;
+        int ways=0;
+        
+        ways=findWays(dp,m-1,n);
+        ways+=findWays(dp,m,n-1);
+      
+        dp[m][n]=ways;
+        return ways;
+    }
 }
