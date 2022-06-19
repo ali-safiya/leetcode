e96e7378f87ae8d7db362382fcfb45f270a9b3f5
ali-safiya <91785905+ali-safiya@users.noreply.github.com>
2022-06-19 13:30:17 +0530

    Time: 5 ms (59.98%), Space: 42.5 MB (47.17%) - LeetHub

--- 264-ugly-number-ii/264-ugly-number-ii.java
+++ 264-ugly-number-ii/264-ugly-number-ii.java
@@ -1,22 +1,23 @@
 class Solution {
     public int nthUglyNumber(int n) {
-        
+       
         int[] ugliNo = new int[n];
         
         ugliNo[0]=1;
+        
         int a=0,b=0,c=0;
         
         for(int i=1;i<n;i++){
             
-            ugliNo[i]=Math.min(ugliNo[a]*2,Math.min(ugliNo[b]*3,ugliNo[c]*5));
+            ugliNo[i]= Math.min( ugliNo[a]*2 ,Math.min( ugliNo[b]*3, ugliNo[c]*5));
             
             if(ugliNo[i]==ugliNo[a]*2)
                 a++;
             if(ugliNo[i]==ugliNo[b]*3)
-                b++;
+                 b++;
             if(ugliNo[i]==ugliNo[c]*5)
                 c++;
-            
+           
         }
         return ugliNo[n-1];
     }
