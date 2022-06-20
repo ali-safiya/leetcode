6f79f915b33c1df2e75bdaa020b168cc844389a7
ali-safiya <91785905+ali-safiya@users.noreply.github.com>
2022-06-20 22:50:42 +0530

    Time: 20 ms (13.41%), Space: 54.5 MB (78.26%) - LeetHub

---
+++ 56-merge-intervals/56-merge-intervals.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int[][] merge(int[][] intervals) {
+       
+        Arrays.sort(intervals,(a,b) -> Integer.compare( a[0],b[0]));
+        
+        LinkedList<int[]> ans = new LinkedList<int[]>();
+        
+        for(int i=0;i<intervals.length;i++){
+            
+            if(ans.isEmpty() || ans.getLast()[1] < intervals[i][0])
+                ans.add(intervals[i]);
+            else
+                ans.getLast()[1]=Math.max(ans.getLast()[1],intervals[i][1]);
+        }
+        
+        return ans.toArray(new int[ans.size()][]);
+    }
+}
