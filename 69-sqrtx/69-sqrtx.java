89be444fd5c0e44f096c543a220c0257323ac1d8
ali-safiya <91785905+ali-safiya@users.noreply.github.com>
2022-09-14 21:20:33 +0530

    Time: 3 ms (39.00%), Space: 41.4 MB (43.74%) - LeetHub

---
+++ 69-sqrtx/69-sqrtx.java
@@ -0,0 +1,27 @@
+class Solution {
+    public int mySqrt(int x) {
+        if(x==0)
+            return 0;
+        long low =1;
+        long high = x;
+        long ans =-1;
+        while(low<=high){
+            
+            long mid=low + (high-low)/2;
+            
+            if(mid*mid==x){
+               return (int)mid; 
+            }
+            else if(mid*mid < x){
+                 ans=mid;
+                low=mid+1;
+                   
+            }
+            else{
+                high=mid-1; 
+            }
+                
+        }
+        return (int)ans;
+    }
+}
