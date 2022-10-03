8bbd879292623727f6235fe17c21449067aa6247
ali-safiya <91785905+ali-safiya@users.noreply.github.com>
2022-10-03 21:21:45 +0530

    Time: 12 ms (16.64%), Space: 54.8 MB (42.98%) - LeetHub

---
+++ 283-move-zeroes/283-move-zeroes.java
@@ -0,0 +1,28 @@
+class Solution {
+    public void moveZeroes(int[] nums) {
+        if(nums.length==1)
+            return ;
+        
+        
+        int i=0;
+        int j=1;
+        
+        while(j<nums.length){
+            
+            if(nums[i]==0 && nums[j]!=0){
+                int temp= nums[i];
+                nums[i]=nums[j];
+                nums[j]=temp;
+                i++;
+                j++;
+            }
+            else if(nums[i] !=0){
+                i++;
+                j=i+1;
+            }
+            else{
+                j++;
+            }
+        }
+    }
+}
