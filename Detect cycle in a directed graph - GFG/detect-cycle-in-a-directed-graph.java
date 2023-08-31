19fff2d110f953c4c48b45ec41a0b58921cd0645
ali-safiya <91785905+ali-safiya@users.noreply.github.com>
2023-08-31 20:24:25 +0530

    Added solution - LeetHub

---
+++ Detect cycle in a directed graph - GFG/detect-cycle-in-a-directed-graph.java
@@ -0,0 +1,76 @@
+//{ Driver Code Starts
+import java.util.*;
+import java.io.*;
+import java.lang.*;
+
+class DriverClass {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+
+        while (t-- > 0) {
+            ArrayList<ArrayList<Integer>> list = new ArrayList<>();
+            int V = sc.nextInt();
+            int E = sc.nextInt();
+            for (int i = 0; i < V; i++)
+                list.add(i, new ArrayList<Integer>());
+            for (int i = 0; i < E; i++) {
+                int u = sc.nextInt();
+                int v = sc.nextInt();
+                list.get(u).add(v);
+            }
+            if (new Solution().isCyclic(V, list) == true)
+                System.out.println("1");
+            else
+                System.out.println("0");
+        }
+    }
+}
+// } Driver Code Ends
+
+
+/*Complete the function below*/
+
+class Solution {
+    // Function to detect cycle in a directed graph.
+    public boolean isCyclic(int V, ArrayList<ArrayList<Integer>> adj) {
+        
+        // code here
+        
+        boolean[] visited = new boolean[V];
+        boolean[] recSt = new boolean[V];
+        
+        for(int i=0;i<V;i++){
+            
+            if(!visited[i]){
+                if(DFS(adj,visited,recSt,i)){
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+    
+    boolean DFS(ArrayList<ArrayList<Integer>> adj, boolean[] visited, boolean[] recSt, int src){
+        
+        
+        recSt[src]=true;
+        visited[src]= true;
+        
+        
+        for(int child : adj.get(src)){
+            
+            if(!visited[child]){
+                if(DFS(adj,visited,recSt,child)){
+                    return true;
+                }
+            }
+            else if(recSt[child]){
+                return true;
+            }
+        }
+        
+        recSt[src]=false;
+        return false;
+    }
+}
