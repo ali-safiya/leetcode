ec673b217fcdc5e5ee2bd5d46989facda7e4906a
ali-safiya <91785905+ali-safiya@users.noreply.github.com>
2026-01-10 01:06:39 +0530

    File updated at 10/01/2026, 01:06:39

--- 73-SetMatrixZeroes/73-SetMatrixZeroes.java
+++ 73-SetMatrixZeroes/73-SetMatrixZeroes.java
@@ -1,56 +1,59 @@
-// Last updated: 20/07/2025, 22:45:59
-class Solution {
-    public void setZeroes(int[][] matrix) {
-
-        int m = matrix.length;
-        int n = matrix[0].length;
-
-        boolean isFirstRowZero = false;
-        boolean isFirstColZero = false;
-
-        for(int i=0;i<m;i++){
-            if(matrix[i][0]== 0) isFirstColZero=true;
-        }
-        for(int i=0;i<n;i++){
-            if(matrix[0][i]== 0) isFirstRowZero=true;
-        }
-
-        for(int i=0;i<m;i++){
-            for(int j=0;j<n;j++){
-                if(matrix[i][j]==0){
-                    matrix[0][j]=0;
-                    matrix[i][0]=0;
-                }
-            }
-        }
-
-        
-        for(int i=1;i<m;i++){
-            if(matrix[i][0]==0){
-                for(int j=1;j<n;j++){
-                    matrix[i][j]=0;
-                }
-            }
-        }
-
-        for(int j=1;j<n;j++){
-            if(matrix[0][j]==0){
-                for(int i=1;i<m;i++){
-                    matrix[i][j]=0;
-                }
-            }
-        }
-
-        if(isFirstColZero){
-            for(int i=0;i<m;i++){
-                matrix[i][0]= 0;
-            }
-        }
-         
-        if(isFirstRowZero){
-            for(int i=0;i<n;i++){
-                matrix[0][i]= 0;
-            }
-        }
-    }
-}
+// Last updated: 10/01/2026, 01:06:39
+1class Solution {
+2    public void setZeroes(int[][] matrix) {
+3
+4        int r = matrix.length;
+5        int c = matrix[0].length;
+6
+7        boolean isRowZero = false;
+8        boolean isColZero = false;
+9
+10        if(matrix[0][0] == 0 ){
+11            isRowZero = true;
+12            isColZero = true;
+13        }
+14
+15        for(int i =0;i<r;i++){
+16            for(int j=0;j<c;j++){
+17
+18                if(i==0 && matrix[i][j]==0){
+19                    isRowZero = true;
+20                }
+21                if(j==0 && matrix[i][j] == 0){
+22                    isColZero = true;
+23                }
+24                
+25                if(matrix[i][j] == 0){
+26                    matrix[i][0] = 0;
+27                    matrix[0][j] = 0;
+28                }
+29            }
+30        }
+31
+32        for(int i =1;i<c;i++){
+33            if(matrix[0][i]==0){
+34                for(int j = 1;j<r;j++){
+35                    matrix[j][i]=0;
+36                }
+37            }
+38        }
+39
+40        for(int i= 1;i<r;i++){
+41            if(matrix[i][0]==0){
+42                for(int j=1;j<c;j++){
+43                    matrix[i][j]=0;
+44                }
+45            }
+46        }
+47        if(isRowZero){
+48            for(int i =0 ;i<c;i++){
+49                matrix[0][i]=0;
+50            }
+51        }
+52        if(isColZero){
+53            for(int i=0;i<r;i++){
+54                matrix[i][0]=0;
+55            }
+56        }
+57    }
+58}
