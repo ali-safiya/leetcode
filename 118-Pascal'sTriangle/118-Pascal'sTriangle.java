d0e0dc5d4db4bd7440534f59b5c200ec514aa721
ali-safiya <91785905+ali-safiya@users.noreply.github.com>
2026-01-09 16:58:14 +0530

    File updated at 09/01/2026, 16:58:14

--- 118-Pascal'sTriangle/118-Pascal'sTriangle.java
+++ 118-Pascal'sTriangle/118-Pascal'sTriangle.java
@@ -1,22 +1,25 @@
-// Last updated: 26/07/2025, 21:13:45
-class Solution {
-    public List<List<Integer>> generate(int numRows) {
-        
-        List<List<Integer>>  tri = new ArrayList<>();
-
-        for(int i=0;i<numRows;i++){
-
-            List<Integer> curRow = new ArrayList<>();
-            for(int j=0;j<i+1;j++){
-                if(j==0 || j==i ){
-                    curRow.add(1);
-                }
-                else{
-                    curRow.add(tri.get(i-1).get(j-1) + tri.get(i-1).get(j));
-                }
-            }
-            tri.add(curRow);
-        }
-        return tri;
-    }
-}
+// Last updated: 09/01/2026, 16:58:14
+1class Solution {
+2    public List<List<Integer>> generate(int numRows) {
+3
+4       List<List<Integer>> ans = new ArrayList<>();
+5       
+6        ans.add(new ArrayList<>(List.of(1)));
+7      
+8        for(int i =1; i<numRows;i++){
+9
+10            List<Integer> row = new ArrayList<>();
+11
+12            row.add(1);
+13            for(int j=1;j<i;j++){
+14
+15                row.add(ans.get(i-1).get(j-1) + ans.get(i-1).get(j));
+16            }
+17            row.add(1);
+18
+19            ans.add(new ArrayList<>(row));
+20        }
+21
+22        return ans;
+23    }
+24}
