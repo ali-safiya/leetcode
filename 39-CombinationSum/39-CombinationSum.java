542d4df7365726f68bc2e0e4754f847fbacc7e10
ali-safiya <91785905+ali-safiya@users.noreply.github.com>
2026-02-09 20:08:09 +0530

    File updated at 09/02/2026, 20:08:08

--- 39-CombinationSum/39-CombinationSum.java
+++ 39-CombinationSum/39-CombinationSum.java
@@ -1,27 +1,24 @@
-// Last updated: 02/08/2025, 22:28:27
-class Solution {
-    public List<List<Integer>> combinationSum(int[] candidates, int target) {
-
-        List<List<Integer>> results = new ArrayList<>();
-        List<Integer> temp = new ArrayList<Integer>();
-        find(candidates,target,results,0,temp);
-        return results;
-        
-    }
-
-    void find(int[] candidates, int target,  List<List<Integer>> results, int index,List<Integer> temp){
-
-        if(index== candidates.length){
-            if(target==0){
-                results.add(new ArrayList<>(temp));
-            }
-            return;
-        }
-        if(candidates[index] <= target){
-            temp.add(candidates[index]);
-            find(candidates,target-candidates[index],results,index,temp);
-            temp.remove(temp.size()-1);
-        }
-        find(candidates,target,results,index+1,temp);
-    }
-}
+// Last updated: 09/02/2026, 20:08:08
+1class Solution {
+2    public List<List<Integer>> combinationSum(int[] candidates, int target) {
+3
+4      List<List<Integer>> ans = new ArrayList<>();
+5      List<Integer> temp =new ArrayList<Integer>();
+6      findSum(candidates,0,target,ans,temp);
+7      return ans;  
+8    }
+9
+10    private void findSum(int[] candidates, int ind, int target, List<List<Integer>> ans, List<Integer> temp){
+11        if(ind>= candidates.length || target<0) return;
+12
+13        if(target==0){
+14            ans.add(new ArrayList<>(temp));
+15            return;
+16        }
+17
+18        temp.add(candidates[ind]);
+19        findSum(candidates,ind,target-candidates[ind],ans,temp);
+20        temp.remove(temp.size()-1);
+21        findSum(candidates,ind+1,target,ans,temp);
+22    }
+23}
