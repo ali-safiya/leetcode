c67dbe8ee816fc6a0c2111c72fd7e99ffac95c2e
ali-safiya <91785905+ali-safiya@users.noreply.github.com>
2026-02-20 03:37:07 +0530

    File updated at 20/02/2026, 03:37:06

--- 40-CombinationSumII/40-CombinationSumII.java
+++ 40-CombinationSumII/40-CombinationSumII.java
@@ -1,35 +1,31 @@
-// Last updated: 03/08/2025, 23:25:08
-class Solution {
-    public List<List<Integer>> combinationSum2(int[] candidates, int target) {
-        
-        Arrays.sort(candidates); 
-        HashSet<List<Integer>> results = new HashSet<>();
-        List<Integer> temp = new ArrayList<>();
-
-        find(results,temp,target,0,candidates);
-        
-        List<List<Integer>> resultList = new ArrayList<>(results);
-
-        return resultList;
-    }
-
-    private void find( HashSet<List<Integer>> results,List<Integer> temp,int target, int index, int[] candidates){
-
-
-         if(target == 0){
-                results.add(new ArrayList<>(temp));
-                return;
-            }
-
-        for(int i= index;i<candidates.length;i++){
-
-            if(i > index && candidates[i]== candidates[i-1]) continue;
-            if(candidates[i]> target) break;
-             
-            temp.add(candidates[i]);
-            find(results,temp,target-candidates[i],i+1, candidates);
-            temp.remove(temp.size()-1);
-        }
-    }
-
-}
+// Last updated: 20/02/2026, 03:37:06
+1class Solution {
+2    public List<List<Integer>> combinationSum2(int[] candidates, int target) {
+3        Arrays.sort(candidates);
+4        List<List<Integer>> ans = new ArrayList<>();
+5        List<Integer> temp = new ArrayList<>();
+6        findComb(candidates,0, target, temp,ans);
+7        return ans;
+8
+9    }
+10
+11    private void findComb(int[] nums, int ind, int target, List<Integer> temp, List<List<Integer>> ans){
+12        
+13
+14        if(target == 0){
+15            ans.add(new ArrayList<>(temp));
+16            return;
+17        }
+18        if(target<0 || ind>=nums.length) return;
+19
+20        for(int i=ind;i<nums.length;i++){
+21            if(i>ind && nums[i]== nums[i-1]) continue;
+22            temp.add(nums[i]);
+23            findComb(nums,i+1,target-nums[i],temp,ans);
+24            temp.remove(temp.size()-1);
+25        }
+26    }
+27
+28
+29
+30}
