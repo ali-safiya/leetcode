914b576ea7da5921e6342515894689fef825b073
ali-safiya <91785905+ali-safiya@users.noreply.github.com>
2026-02-20 03:16:12 +0530

    File updated at 20/02/2026, 03:16:11

--- 90-SubsetsII/90-SubsetsII.java
+++ 90-SubsetsII/90-SubsetsII.java
@@ -1,27 +1,28 @@
-// Last updated: 04/08/2025, 13:19:22
-class Solution {
-    public List<List<Integer>> subsetsWithDup(int[] nums) {
-        Arrays.sort(nums);
-        List<List<Integer>> results = new ArrayList<>();
-        List<Integer> temp = new ArrayList<>();
-
-        findSubsets(results,nums,temp,0);
-        return results;
-    }
-
-    private void findSubsets(List<List<Integer>> results,int[] nums,List<Integer> temp , int index){
-
-        if(index > nums.length) return;
-        results.add(new ArrayList<>(temp));
-
-
-        for(int i= index;i<nums.length;i++){
-
-            if(i > index && nums[i] == nums[i-1]) continue;
-
-            temp.add(nums[i]);
-            findSubsets(results,nums,temp,i+1);
-            temp.remove(temp.size()-1);
-        }
-    }
-}
+// Last updated: 20/02/2026, 03:16:11
+1class Solution {
+2    public List<List<Integer>> subsetsWithDup(int[] nums) {
+3
+4        Arrays.sort(nums);
+5        List<List<Integer>> ans = new ArrayList<>();
+6        List<Integer> temp = new ArrayList<>();
+7        findSubsets(nums,0, ans, temp);
+8        return ans;
+9        
+10    }
+11
+12    private void findSubsets(int[] nums, int ind, List<List<Integer>> ans , List<Integer> temp){
+13        if(ind > nums.length) return;
+14        ans.add(new ArrayList<>(temp));
+15
+16
+17        for(int i= ind;i<nums.length;i++){
+18
+19            if(i>ind && nums[i] == nums[i-1]) continue;
+20            temp.add(nums[i]);
+21            findSubsets(nums,i+1,ans,temp);
+22            temp.remove(temp.size()-1);
+23        }
+24    }
+25
+26    
+27}
