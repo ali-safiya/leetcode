17ba38cc363085e916a1aa8038c2ec8567122ed2
ali-safiya <91785905+ali-safiya@users.noreply.github.com>
2026-02-21 03:13:15 +0530

    File updated at 21/02/2026, 03:13:14

--- 46-Permutations/46-Permutations.java
+++ 46-Permutations/46-Permutations.java
@@ -1,34 +1,32 @@
-// Last updated: 05/08/2025, 13:57:18
-class Solution {
-    public List<List<Integer>> permute(int[] nums) {
-
-        List<List<Integer>> results = new ArrayList<>();
-        List<Integer> temp = new ArrayList<>();
-        find(nums,results,0,temp);
-        return results;
-    }
-
-    private void find(int[] nums,List<List<Integer>> results,int index, List<Integer> temp ){
-
-        if(index==nums.length){
-            results.add(new ArrayList<>(temp));
-            return;
-        }
-        if(index > nums.length) return;
-
-        for(int i= index;i<nums.length;i++){
-            swap(i,index,nums);
-            temp.add(nums[index]);
-            find(nums,results,index+1,temp);
-            temp.remove(temp.size()-1);
-            swap(i,index,nums);
-        }
-    }
-
-    private void swap(int i, int j , int[] nums){
-
-        int temp=nums[i];
-        nums[i] = nums[j];
-        nums[j] = temp;
-    }
-}
+// Last updated: 21/02/2026, 03:13:14
+1class Solution {
+2    public List<List<Integer>> permute(int[] nums) {
+3
+4        List<List<Integer>> results = new ArrayList<>();
+5        find(nums,results,0);
+6        return results;
+7    }
+8
+9    private void find(int[] nums,List<List<Integer>> results,int index ){
+10
+11        if(index== nums.length){
+12            List<Integer> temp = new ArrayList<>();
+13            for(int num: nums) temp.add(num);
+14            results.add(temp);
+15            return;
+16        }
+17
+18        for(int i=index;i<nums.length;i++){
+19            swap(index,i,nums);
+20            find(nums,results,index+1);
+21            swap(index,i,nums);
+22        }
+23    }
+24
+25    private void swap(int i, int j , int[] nums){
+26
+27        int temp=nums[i];
+28        nums[i] = nums[j];
+29        nums[j] = temp;
+30    }
+31}
