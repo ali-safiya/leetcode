20b5b1dec424bf257861d4bfe88c39d1ce8b1efe
ali-safiya <91785905+ali-safiya@users.noreply.github.com>
2026-03-01 01:34:18 +0530

    File updated at 01/03/2026, 01:34:18

--- 235-LowestCommonAncestorofaBinarySearchTree/235-LowestCommonAncestorofaBinarySearchTree.java
+++ 235-LowestCommonAncestorofaBinarySearchTree/235-LowestCommonAncestorofaBinarySearchTree.java
@@ -1,4 +1,4 @@
-// Last updated: 01/03/2026, 01:32:16
+// Last updated: 01/03/2026, 01:34:18
 1/**
 2 * Definition for a binary tree node.
 3 * public class TreeNode {
@@ -13,27 +13,14 @@
 12    public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
 13
 14        if(root == null) return null;
-15
-16        TreeNode left = null;
-17        TreeNode right = null;
-18
-19        if(root.val == p.val || root.val == q.val){
-20            return root;
+15        
+16        if(root.val > p.val && root.val > q.val){
+17            return lowestCommonAncestor(root.left,p,q);
+18        }
+19        else if(root.val <p.val && root.val < q.val){
+20            return lowestCommonAncestor(root.right,p,q);
 21        }
-22        if(root.val > p.val && root.val > q.val){
-23            left = lowestCommonAncestor(root.left,p,q);
-24        }
-25        else if(root.val <p.val && root.val < q.val){
-26            right = lowestCommonAncestor(root.right,p,q);
-27        }
-28        else{
-29            left= lowestCommonAncestor(root.left,p,q);
-30            right= lowestCommonAncestor(root.right,p,q);
-31        }
-32
-33        if(left != null && right != null) return root;
-34
-35        return left==null ? right : left ;
-36        
-37    }
-38}
+22        return root;
+23        
+24    }
+25}
