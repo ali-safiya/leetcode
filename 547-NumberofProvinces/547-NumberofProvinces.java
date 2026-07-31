94a118704a601e14e695fe08cdf61f05a92d7c08
ali-safiya <91785905+ali-safiya@users.noreply.github.com>
2026-07-31 20:18:39 +0530

    File updated at 31/07/2026, 20:18:39

--- 547-NumberofProvinces/547-NumberofProvinces.java
+++ 547-NumberofProvinces/547-NumberofProvinces.java
@@ -1,30 +1,51 @@
-// Last updated: 05/03/2026, 02:54:56
+// Last updated: 31/07/2026, 20:18:39
 1class Solution {
 2    public int findCircleNum(int[][] isConnected) {
 3        
 4        int n = isConnected.length;
-5
-6        boolean[] visited = new boolean[n];
-7        int provinces=0;
-8        for(int i=0;i<n;i++){
-9            if(!visited[i]){
-10                dfs(isConnected,i,visited,n);
-11                provinces++;
-12            }
-13        } 
-14        return provinces;
-15    }
-16
-17    private void dfs(int[][] isConnected, int i, boolean[] visited, int n){
-18        if(i> n || visited[i]) return;
-19
-20        visited[i] = true;
-21
-22        for(int j =0;j<n ;j++){
-23            if(isConnected[i][j] ==1){
-24                dfs(isConnected,j,visited,n);
-25            }
-26        }
-27
-28    }
-29}
+5        int[] size = new int[n];
+6        int[] parent = new int[n];
+7        int noOfComp =n;
+8
+9        for(int i=0;i<n;i++){
+10            size[i]=1;
+11            parent[i]=i;
+12        }
+13
+14        for(int i=0;i<n;i++){
+15            for(int j=i+1;j<n;j++){
+16                if(isConnected[i][j]==1 && union(i,j,size,parent)){
+17                    noOfComp--;
+18                }
+19            }
+20        }
+21        return noOfComp;
+22    }
+23
+24    private boolean union(int i , int j,int[] size, int[] parent){
+25
+26        int x = find(i,parent);
+27        int y = find(j,parent);
+28
+29        if(x==y) return false;
+30
+31        if(size[x] < size[y]){
+32            parent[x] = y;
+33            size[y] += size[x];
+34        }else{
+35            parent[y] = x;
+36            size[x] += size[y];
+37        }
+38        return true;
+39    }
+40
+41    private int find(int x, int[] parent){
+42
+43        if(parent[x]!=x){
+44            parent[x]=find(parent[x],parent);
+45        }
+46        return parent[x];
+47    }
+48
+49
+50}
